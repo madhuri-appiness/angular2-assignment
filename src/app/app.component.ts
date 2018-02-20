08f5360d92fe9e28c12760ef676e916056786214
madhuri-appiness <madhuridp298@gmail.com>
2018-02-20 23:06:09 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, trigger,state,style,animate,transition } from '@angular/core';
+import * as firebase from 'firebase';
+
+export const config = {
+  apiKey: "AIzaSyCl2eukDSODz-a4aVLtg24UzZmqOZqlTVQ",
+    authDomain: "assignment-20fa8.firebaseapp.com",
+    databaseURL: "https://assignment-20fa8.firebaseio.com",
+    projectId: "assignment-20fa8",
+    storageBucket: "assignment-20fa8.appspot.com",
+    messagingSenderId: "405633600522"
+};
+// import { AngularFire,AuthProviders,AuthMethods, FirebaseListObservable } from 'angularfire2';
+export const ACCESS_CONFIG = firebase.initializeApp(config);
+
+const rootRef = ACCESS_CONFIG.database().ref().child("assignment-20fa8");
+const database = firebase.database().ref('form-value');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations :[
+    trigger('momenttext',[
+      transition('void => *',[
+        style({transform :'translateX(-5%)'
+      }),
+        animate('1000ms ease-out')
+      ]
+    )
+    ])
+
+  ]
 })
 export class AppComponent {
   title = 'app';
+ data= {};
+ 
+ formSubmit(){
+   database.once('value')
+   .then((snapshot)=>{
+    let arr = snapshot.val();
+    if(arr){
+      arr.push(this.data);
+    }else{
+      arr = [];
+    }
+    database.set(arr);
+   });
+  // database.set([this.data]);
+  //  
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import * as firebase from 'firebase';
+import { loadavg } from 'os';
+
+
+
+
+
+
+// firebase.database().ref('form-value').set([{
+//   name:"madhuri",
+//   patner : "Aashish"
+// }]);
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
